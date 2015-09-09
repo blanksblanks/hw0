8d1e9c536d7844fe5b2bcc6260a8f01d87a062ea
Nina Baculinao <nb2406@columbia.edu>
2015-09-09 23:02:04 +0000

    wrap functionality in functions

--- scotch.py
+++ scotch.py
@@ -1,12 +1,18 @@
 import csv
 
-input_file = open("iowa-liquor-sample.csv")
-csv_file = csv.reader(input_file, delimiter=',')
-num_whiskies = 0 
-for row in csv_file:
-    for entry in row:
-        if "single malt scotch" in entry.lower():
-            num_whiskies += 1
-            break
-print num_whiskies
+def file_reader(file_path):
+    input_file = open(file_path)
+    return csv.reader(input_file, delimiter=',')
 
+def count_whiskey_apperance(csv):
+    num_whiskies = 0 
+    for row in csv:
+        for entry in row:
+            if "single malt scotch" in entry.lower():
+                num_whiskies += 1
+                break
+    print num_whiskies
+
+if __name__ == '__main__':
+    csv = file_reader("iowa-liquor-sample.csv")
+    count_whiskey_apperance(csv)
